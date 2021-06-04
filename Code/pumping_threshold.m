8b366a8b235029417cb47098db6442cd4233c201
Frank01001 <xfrank01001@gmail.com>
2021-06-04 10:28:30 +0200

    Uploaded pumping_threshold prototype

---
+++ Code/pumping_threshold.m
@@ -0,0 +1,152 @@
+clear; % clear the workspace
+close all; % closes the windows
+
+%Constants
+TIME_STEPS = 200;
+LATTICE_WIDTH = 100;
+LATTICE_HEIGHT = 100;
+PHOTON_SATURATION = 20;
+
+%Initialize System
+cell.electron = 0;
+cell.electronLife = 0;
+cell.photonCount = 0;
+cell.lifeTimes = zeros(1, PHOTON_SATURATION);
+
+currAutomaton = repmat(cell, LATTICE_WIDTH, LATTICE_HEIGHT);
+prevAutomaton = currAutomaton;
+
+% Oscillatory behavior input data
+electronLifeTime = 10;%logspace(1, 2.3, 10);
+photonLifeTime = 20;
+pumpingProbabilitySpace = logspace(-3, -1, 10);
+noiseProbability = 5e-5;
+stimulatedEmissionThreshold = 1;
+
+%output data
+populationCounter = zeros(1, TIME_STEPS);
+photonCounter = zeros(1, TIME_STEPS);
+
+for p = 1:length(pumpingProbabilitySpace)
+    pumpingProbability = pumpingProbabilitySpace(p);
+    noisePhotons = zeros(1, TIME_STEPS);
+    
+    %Time iteration
+    for t = 1:TIME_STEPS
+        %Noise probability matrix
+        noise = rand(LATTICE_WIDTH, LATTICE_HEIGHT);
+
+        populationSum = 0;
+        photonSum = 0;
+
+        %For each cell
+        for i = 1:LATTICE_WIDTH
+            for j = 1:LATTICE_HEIGHT
+
+                %Apply stimulated emission rule
+                if currAutomaton(i, j).electron == 1 && currAutomaton(i, j).photonCount < PHOTON_SATURATION && ...
+                        mooreNeighborhood(prevAutomaton, i, j, LATTICE_WIDTH, LATTICE_HEIGHT) >= stimulatedEmissionThreshold
+                    for index = 1:PHOTON_SATURATION
+                        if currAutomaton(i, j).lifeTimes(index) == 0
+                            currAutomaton(i, j).lifeTimes(index) = photonLifeTime;
+                            currAutomaton(i, j).photonCount = currAutomaton(i, j).photonCount + 1; 
+                            break;
+                        end
+                    end
+                    currAutomaton(i, j).electron = 0;
+                    currAutomaton(i, j).electronLife = 0;
+                end
+
+                %Apply photon decay
+                for index = 1:PHOTON_SATURATION
+                    if currAutomaton(i, j).lifeTimes(index) > 0
+                        currAutomaton(i, j).lifeTimes(index) = currAutomaton(i, j).lifeTimes(index) - 1; 
+                        if currAutomaton(i, j).lifeTimes(index) == 0
+                            currAutomaton(i, j).photonCount = currAutomaton(i, j).photonCount - 1; 
+                        end
+                    end
+                end
+
+                %Apply electron decay
+                if currAutomaton(i, j).electron == 1 && currAutomaton(i, j).electronLife > 0
+                    currAutomaton(i, j).electronLife = currAutomaton(i, j).electronLife - 1; 
+                    if currAutomaton(i, j).electronLife == 0
+                        currAutomaton(i, j).electron = 0;
+                    end
+                end
+
+                %Apply pumping rule
+                if currAutomaton(i, j).electron == 0 && rand < pumpingProbability
+                    currAutomaton(i, j).electron = 1;
+                    currAutomaton(i, j).electronLife = electronLifeTime;
+                end
+
+                %Apply noise photon
+                if noise(i, j) < noiseProbability
+                    for index = 1:PHOTON_SATURATION
+                        if currAutomaton(i, j).lifeTimes(index) == 0
+                            currAutomaton(i, j).lifeTimes(index) = photonLifeTime;
+                            currAutomaton(i, j).photonCount = currAutomaton(i, j).photonCount + 1;
+                            noisePhotons(t) = noisePhotons(t) + 1;
+                            break;
+                        end
+                    end
+                end
+
+                %Partial sum of upper state electrons
+                populationSum = populationSum + currAutomaton(i, j).electron;
+                %Partial sum of total photons in the lattice
+                photonSum = photonSum + currAutomaton(i, j).photonCount;
+            end
+        end
+       
+        %Calculate populations after this time step
+        populationCounter(t) = populationSum;
+        photonCounter(t) = photonSum;
+
+        %Update state
+        prevAutomaton = currAutomaton;
+    end
+    
+    n_np = mean(noisePhotons) * photonLifeTime;
+    
+    averagePhotons = mean(photonCounter);
+    
+    if(averagePhotons > 1.25 * n_np)
+    	fprintf("Threshold found! ?_th = %d\n", pumpingProbability);
+    end
+    
+    fprintf("Ended simulation n.%d : n_np = %d : ? = %d\n", p, n_np, pumpingProbability);
+end
+
+%Final calculations
+%Output results
+figure("Name", "First graph");
+grid on;
+hold on;
+title("Population inversion over time");
+time = linspace(1, TIME_STEPS, TIME_STEPS);
+plot(time, populationCounter);
+plot(time, photonCounter);
+legend('Population Inversion', 'Photon Count');
+xlabel('Time Step');
+ylabel('Population');
+hold off;
+
+
+%Returns the number of photons in the adjacent cells using moore's
+%neighborhood rule
+function count = mooreNeighborhood(automaton, center_x, center_y, lattice_width, lattice_height)
+    count = 0;
+    
+    min_x = max(center_x - 1, 1);
+    max_x = min(center_x + 1, lattice_width);
+    min_y = max(center_y - 1, 1);
+    max_y = min(center_y + 1, lattice_height);
+    
+    for i = min_x:max_x
+        for j = min_y:max_y
+            count = count + automaton(i, j).photonCount;
+        end
+    end
+end
