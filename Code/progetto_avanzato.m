09e9e997c865df4264667815bbec87bcd3a10044
Frank01001 <xfrank01001@gmail.com>
2021-06-09 11:03:02 +0200

    Updated Project

--- Code/progetto_avanzato.m
+++ Code/progetto_avanzato.m
@@ -1,11 +1,36 @@
 clear; % clear the workspace
 close all; % closes the windows
 
-%Constants
-TIME_STEPS = 1000;
-LATTICE_WIDTH = 50;
-LATTICE_HEIGHT = 50;
-PHOTON_SATURATION = 30;
+%Data insertion
+TIME_STEPS = input("Enter simulation duration in ps:");
+accuracy = input("Type in simulation accuracy (Low, Medium, High, Ultra):", "s");
+PHOTON_SATURATION = input("Enter maximum number of photons contained in a cell:");
+electronLifeTime = input("Insert carrier life time in ps:");
+photonLifeTime = input("Insert cavity life time in ps:");
+pumpingRate = input("Enter Pumping rate (atoms/s):");
+thermalExcitingProbability = input("Enter probability that in each time step thermal excitation can occur:");
+spontaneousEmissionProbability = input("Enter probability that in each time step spontaneous emission can occur:");
+stimulatedEmissionThreshold = 1;
+
+%Choose lattice dimensions
+switch(lower(accuracy))
+    case "low"
+        LATTICE_WIDTH = 50;
+        LATTICE_HEIGHT = 50;
+    case "medium"
+        LATTICE_WIDTH = 150;
+        LATTICE_HEIGHT = 150;
+    case "high"
+        LATTICE_WIDTH = 300;
+        LATTICE_HEIGHT = 300;
+    case "ultra"
+        LATTICE_WIDTH = 400;
+        LATTICE_HEIGHT = 400;
+end
+
+%Calculate pumping
+pumpingProbability = (pumpingRate * 10^(-12)) / (LATTICE_WIDTH * LATTICE_HEIGHT);
+fprintf("Pumping prob. set at %d\n", pumpingProbability);
 
 %Initialize System
 cell.electron = 0;
@@ -16,18 +41,13 @@ cell.lifeTimes = zeros(1, PHOTON_SATURATION);
 currAutomaton = repmat(cell, LATTICE_WIDTH, LATTICE_HEIGHT);
 prevAutomaton = currAutomaton;
 
-% Constant behavior input data
-electronLifeTime = 30;
-photonLifeTime = 10;
-pumpingProbability = 4e-3;
-stimulatedEmissionThreshold = 1;
-thermalExcitingProbability = 1e-4;
-spontaneousEmissionProbability = 5e-4;
-
-
 %output data
 populationCounter = zeros(1, TIME_STEPS);
 photonCounter = zeros(1, TIME_STEPS);
+spontaneousEmissionPhotons = zeros(1, TIME_STEPS);
+thermalExcitingElectrons = zeros(1, TIME_STEPS);
+
+wb = waitbar(0, "Simulation Progress");
 
 %Time iteration
 for t = 1:TIME_STEPS
@@ -83,13 +103,15 @@ for t = 1:TIME_STEPS
             if currAutomaton(i, j).electron == 0 && noise(i, j) < thermalExcitingProbability
             	currAutomaton(i, j).electron = 1;
             	currAutomaton(i, j).electronLife = electronLifeTime;
+                thermalExcitingElectrons(t) = thermalExcitingElectrons(t) + 1;
             elseif currAutomaton(i, j).electron == 1 && noise(i, j) < spontaneousEmissionProbability
-                currAutomaton(i, j).electron = 0;
-                currAutomaton(i, j).electronLife = 0;
                 for index = 1:PHOTON_SATURATION
                     if currAutomaton(i, j).lifeTimes(index) == 0
+                        currAutomaton(i, j).electron = 0;
+                        currAutomaton(i, j).electronLife = 0;
                         currAutomaton(i, j).lifeTimes(index) = photonLifeTime;
-                        currAutomaton(i, j).photonCount = currAutomaton(i, j).photonCount + 1; 
+                        currAutomaton(i, j).photonCount = currAutomaton(i, j).photonCount + 1;
+                        spontaneousEmissionPhotons(t) = spontaneousEmissionPhotons(t) + 1;
                         break;
                     end
                 end
@@ -106,20 +128,32 @@ for t = 1:TIME_STEPS
     populationCounter(t) = populationSum;
     photonCounter(t) = photonSum;
     
+    progressString = sprintf("Time %d ps | %3.2f%%", t, (t / TIME_STEPS * 100));
+    waitbar((t / TIME_STEPS), wb, progressString);
+    
     %Update state
     prevAutomaton = currAutomaton;
 end
 
+%steadyStatePopulationSamples = populationCounter(1, (TIME_STEPS * 3 / 4):TIME_STEPS);
+%steadyStatePopulation = mean(steadyStatePopulationSamples);
+%K = 1 / (steadyStatePopulation * photonLifeTime);
+%pumpingRateThr = 1 / (K * photonLifeTime * electronLifeTime);
+%fprintf("The estimated K is %d\n", K);
+%fprintf("The estimated pumping rate threshold is %d\n", pumpingRateThr);
+
 %Final calculations
 %Output results
-figure("Name", "First graph");
+figure(1);
 grid on;
 hold on;
 title("Population inversion over time");
 time = linspace(1, TIME_STEPS, TIME_STEPS);
 plot(time, populationCounter);
 plot(time, photonCounter);
-legend('Population Inversion', 'Photon Count');
+plot(time, spontaneousEmissionPhotons);
+plot(time, thermalExcitingElectrons)
+legend('Population Inversion', 'Photon Count', 'Spontaneous Emission', 'Thermal Agitation');
 xlabel('Time Step');
 ylabel('Population');
 hold off;
